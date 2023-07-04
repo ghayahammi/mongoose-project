9fa9ef4179ac0a0807efdd105a302bbecf302a90
ghaya <hammighaya@gmail.com>
2023-07-04 10:17:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require("express")
+const connectDB=require("./config/connectDB")
+
+const app=express()
+app.use(express.json())
+connectDB()
+const port=7000
+
+app.use("/api/contact",require("./routes/contact"))
+app.listen(port,(err)=>{
+err?console.log(err):console.log(`server is running on port ${port}`)
+
+})
